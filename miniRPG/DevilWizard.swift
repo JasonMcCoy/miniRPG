9344dbdf5e77599c6e16045d3806ba204be98287
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-06-15 10:17:08 -0700

    Finished with DevilWizard class
    Okay, honestly I’m done for now.
    
    Will finish UI and rest of app tonight. PEACE!

--- miniRPG/DevilWizard.swift
+++ miniRPG/DevilWizard.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
-class DevilWizard {
+class DevilWizard: Enemy {
+    
+    override var loot: [String] {
+        return ["Magic Wand", "Dark Amulet", "Salted Pork"]
+    }
+    
+    override var type: String {
+        return "Devil Wizard"
+    }
     
 }

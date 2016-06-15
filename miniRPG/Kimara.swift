c72fc4abcc00c145ae87637893bb4c93b15ed77a
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-06-15 09:55:48 -0700

    Finished Kimara class
    All that’s left is the DevilWizard class and setting up images &
    constraints.
    
    Will finish tonight.

--- miniRPG/Kimara.swift
+++ miniRPG/Kimara.swift
@@ -8,6 +8,24 @@
 
 import Foundation
 
-class Kimara {
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom", "Rare Trident"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr: attackPwr)
+        } else {
+            return false
+        }
+    }
     
 }

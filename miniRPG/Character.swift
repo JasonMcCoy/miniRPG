8cc100eb897f84a910d3fb1a646bb13af4abb3d8
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-06-15 09:29:02 -0700

    Finished Character class code
    Next the Player class.

--- miniRPG/Character.swift
+++ miniRPG/Character.swift
@@ -10,4 +10,39 @@ import Foundation
 
 class Character {
     
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 10
+    
+    var attackPwr: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init(startingHp: Int, attackPwr: Int) {
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        return true
+    }
+    
 }

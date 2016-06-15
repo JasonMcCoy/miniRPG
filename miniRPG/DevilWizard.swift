59157aedf081f379c54a6ad962364c83a7199ba4
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-06-15 09:10:39 -0700

    Setup all classes
    Added all 5 classes for the game:
    1. Character
    2. Player
    3. Enemy
    4. Kimara
    5. DevilWizard
    
    Next is to add all images for UI.

---
+++ miniRPG/DevilWizard.swift
@@ -0,0 +1,13 @@
+//
+//  DevilWizard.swift
+//  miniRPG
+//
+//  Created by Jason McCoy on 6/15/16.
+//  Copyright © 2016 Jason McCoy. All rights reserved.
+//
+
+import Foundation
+
+class DevilWizard {
+    
+}

930b8d31fbc9c8229545cb0c816a2b2e04b398fa
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-06-15 09:47:31 -0700

    Finished Enemy class
    Next is the Kimara class.

--- miniRPG/Enemy.swift
+++ miniRPG/Enemy.swift
@@ -10,6 +10,22 @@ import Foundation
 
 class Enemy: Character {
     
+    var loot: [String] {
+        return ["Rusty Dagger", "Cracked Buckler"]
+    }
     
+    var type: String {
+        return "Grunt"
+    }
+    
+    func dropLoot() -> String? {
+        if !isAlive {
+            let rand = Int(arc4random_uniform(UInt32(loot.count)))
+            return loot[rand]
+        }
+        
+        return nil
+        
+    }
     
 }
